31b7e31274c5da468234436505708028d073bd1e
vmuliawan <vmuliawan29@gmail.com>
2023-07-22 22:46:48 +0700

    fix vacancies & develop notif modal popup

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,8 +4,10 @@ const linkSidebar = document.querySelectorAll(".text-link");
 const iconSidebar = document.querySelectorAll(".icon-sidebar");
 const backgroundLogoSidebar = document.querySelectorAll(".bg-logo-sidebar");
 const modalNotif = document.querySelector(".container-notif");
-const vacancies = document.querySelectorAll(".vacancies");
+const vacancies = document.querySelectorAll(".col-vacancies");
 const btnSeemore = document.querySelector(".btn-seemore");
+const vacancies2 = document.querySelector("col-vacancies-2");
+
 document.querySelector(".hamburger-menu").onclick = function () {
   sidebar.classList.toggle("sidebar-small");
   mainContent.classList.toggle("container-page-large");
@@ -23,12 +25,19 @@ document.querySelector(".hamburger-menu").onclick = function () {
   vacancies.forEach((vl) => {
     vl.classList.toggle("col-5");
   });
+  vacancies2.classList.toggle("col-vacancies-toggle");
 };
 
-document.querySelector(".btn-notif").onclick = function () {
+document.querySelector(".btn-notif").addEventListener("click", function () {
   if (modalNotif.style.display === "block") {
     modalNotif.style.display = "none";
   } else {
     modalNotif.style.display = "block";
+  }
+});
+
+window.onclick = function (event) {
+  if (event.target == modalNotif) {
+    modalNotif.style.display = "none";
   }
 };
