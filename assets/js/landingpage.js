3d335d7c28f208d875dbc906f958035969dca7f5
vmuliawan <vmuliawan29@gmail.com>
2023-07-22 14:07:12 +0700

    improve status button login as mahasiswa or dosen

--- assets/js/landingpage.js
+++ assets/js/landingpage.js
@@ -2,10 +2,10 @@ const loginMahasiswa = document.querySelector(".login-mahasiswa");
 const loginDosen = document.querySelector(".login-dosen");
 
 document.querySelector(".login-mahasiswa").onclick = function () {
-  loginMahasiswa.classList.add("btn-status-active");
-  loginDosen.classList.remove("btn-status-active");
+  loginMahasiswa.classList.add("active");
+  loginDosen.classList.remove("active");
 };
 loginDosen.onclick = function () {
-  loginDosen.classList.add("btn-status-active");
-  loginMahasiswa.classList.remove("btn-status-active");
+  loginDosen.classList.add("active");
+  loginMahasiswa.classList.remove("active");
 };
